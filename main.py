8f7b3d8be5f861d1ac2cbeda94644e2acde5ae11
Ilusinusmate <joaogabrielsalvadorpaiva@gmail.com>
2025-09-29 01:03:59 -0300

    feat: env file and port set to onrender.com

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from src.app import app
-import src.controllers.runner
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 
 import uvicorn
 
+from src.app import app
+import src.controllers.runner
+
+
+ENV = load_dotenv(Path(__file__).parent)
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=os.environ.get("PORT", 10000))
